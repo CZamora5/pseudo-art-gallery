4c1185f9e7bda85680ccda42f6b0166b02f8f71a
Carlos Zamora <czamora5@ucol.mx>
2022-10-18 04:56:44 -0500

    feat: include paintings data

---
+++ data.js
@@ -0,0 +1,67 @@
+const paintings = [
+  {
+    name: 'The birth of Venus',
+    year: '1863',
+    details: 'Oil on canvas',
+    dimensions: '150 cm x 250 cms',
+    image: './assets/the-birth-of-venus.jpg'
+  },
+  {
+    name: 'Portrait of an arab',
+    year: '1875',
+    details: 'Oil on canvas',
+    dimensions: '65 x 54 cms',
+    image: './assets/portrait-of-an-arab.jpg'
+  },
+  {
+    name: 'Albayde',
+    year: '1848',
+    details: 'Oil on canvas',
+    dimensions: '98 x 80 cms',
+    image: './assets/albayde.jpg'
+  },
+  {
+    name: 'Giacomina',
+    year: '1871',
+    details: 'Oil on canvas',
+    dimensions: '160 x 76.2 cms',
+    image: './assets/giacomina.jpg'
+  },
+  {
+    name: 'Cleopatra testing poisons on condemned prisoners',
+    year: '1887',
+    dimensions: '165 cm x 290 cms',
+    details: 'Oil on canvas',
+    image: './assets/cleopatra-testing-poisons.jpg'
+  },
+  {
+    name: 'Love\'s messenger',
+    year: '1883',
+    details: 'Oil on canvas',
+    dimensions: '47.6 x 80.6 cms',
+    image: './assets/loves-messenger.jpg'
+  },
+  {
+    name: 'The death of Moses',
+    year: '1851',
+    details: 'Oil on canvas',
+    dimensions: '284.5 x 304.8 cms',
+    image: './assets/the-death-of-moses.jpg'
+  },
+  {
+    name: 'Fallen angel',
+    year: '1847',
+    details: 'Oil on canvas',
+    dimensions: '121 x 189.7 cms',
+    image: './assets/fallen-angel.jpg'
+  },
+  {
+    name: 'Cincinnatus receiving deputies of the senate',
+    year: '1844',
+    details: 'Oil on canvas',
+    dimensions: '114.3 x 146.4 cms',
+    image: './assets/cincinnatus.jpeg'
+  }
+];
+
+export default paintings;
